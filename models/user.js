0daa7469d4366b681371530e0c8c52c034d024fa
framista <adajab1@gmail.com>
2022-01-05 23:23:28 +0100

    Store date with user

--- models/user.js
+++ models/user.js
@@ -1,19 +1,22 @@
 const mongoose = require('mongoose');
 
-const userSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    user_id: {
+      type: String,
+    },
   },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  user_id: {
-    type: String,
-  },
-});
+  { timestamps: { createdAt: 'created_at' } }
+);
 
 const User = mongoose.model('User', userSchema);
 
